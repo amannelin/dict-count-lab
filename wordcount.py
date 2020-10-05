c8afbbc824220a272e0c184262c5f092e483e40f
amannelin <amannelin@gmail.com>
2020-10-05 16:40:26 +0000

    word count without using functions Becca and Abby

---
+++ wordcount.py
@@ -0,0 +1,16 @@
+
+poem = open('twain.txt')   
+word_counts = {}
+    
+    
+    
+for line in poem:
+    words = line.rstrip().split(" ")
+    
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+
+print(word_counts)
+    
+
+
---
+++ wordcount_functions.py
@@ -0,0 +1,5 @@
+#def make_word_count(the_file):
+
+
+
+#make_word_count("test.txt")
